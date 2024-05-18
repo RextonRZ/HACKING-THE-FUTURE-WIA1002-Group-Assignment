9dc2b613cbed37386a7f04422a718a748b1bd28c
ruizh <ruizh@LAPTOP-NT1TJVTA>
2024-05-18 23:41:54 +0800

    Update Login Controller

--- src/main/java/com/example/demo/loginController.java
+++ src/main/java/com/example/demo/loginController.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
@@ -9,15 +14,17 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 
 public class loginController {
     @FXML
     private Stage stage;
     @FXML
-    private TextField useremailLogin;
+    private TextField usernameTextField;
     @FXML
-    private TextField passwordLogin;
+    private TextField passwordField;
 
 
     @FXML
@@ -30,8 +37,40 @@ public class loginController {
 
     @FXML
     public void loginButton(ActionEvent event) throws Exception{
-        homeController homeController = new homeController();
-        homeController.homeButton(event);
+        
+        if (authenticate()){
+            Parent root2 = FXMLLoader.load(getClass().getResource("homePage.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene homeScene = new Scene(root2, stage.getScene().getWidth(), stage.getScene().getHeight());
+            stage.setScene(homeScene);
+        }else{
+            displayLoginError();
+        }
+        
+    }
+    
+    private boolean authenticate(){
+        
+        try(Connection SQL = DriverManager.getConnection("**jdbc:mysql://**","**Username**", "**Password**")){
+            String query = "SELECT * FROM users WHERE username=? AND passwords=?";
+            try(PreparedStatement statement = SQL.prepareStatement(query)){
+                statement.setString(1, usernameTextField.getText());
+                statement.setString(2, passwordField.getText());
+                try (ResultSet result = statement.executeQuery()){
+                    return result.next();
+                }
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    private void displayLoginError(){
+        Alert alertLogin = new Alert(AlertType.ERROR);
+        alertLogin.setTitle("Login Failed");
+        alertLogin.setHeaderText(null);
+        alertLogin.setContentText("Invalid username or password. Please try again later!");
     }
 
     @FXML
